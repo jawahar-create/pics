93f7e7b8983a6e0935ee15c0d245fca1fadf42b8
Nagendra203 <dammatinagendrababu.marolix@gmail.com>
2023-10-19 00:28:21 +0530

    first commit

---
+++ slash/etc/sessionsaliases.sh
@@ -0,0 +1,3 @@
+function Jenkins() {
+"/home/mobaxterm/LauncherFolder/MobaXterm.exe" -bookmark2 " #109#0%65.1.248.209%22%ubuntu%%-1%-1%%%%%0%0%0%_ProfileDir_\Downloads\jenkins.pem%%-1%0%0%0%%1080%%0%0%1%%0%%%%0%-1%-1%0#MobaFont%10%0%0%-1%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%0%_Std_Colors_0_%80%24%0%1%-1%<none>%%0%0%-1%0#0# #-1"
+}
